b12bdb5ce1aa3da37e88503537df456ad50371dd
Naveen Anil <naveenms01@gmail.com>
2025-03-06 10:49:31 +0000

    OPS: Initial commit

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Install dependencies
+poetry install --with dev
+
+# Auto set up remote when pushing new branches
+git config --global --add push.autoSetupRemote 1
+
+git config --global --add safe.directory /workspaces/django-resilient-webhook
+
+# Install precommit hooks
+pre-commit install && pre-commit install -t commit-msg
