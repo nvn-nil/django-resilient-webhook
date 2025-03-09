25741dc9b071752c7a69b454e1beb68f26a937b9
Naveen Anil <devons.horizon@gmail.com>
2025-03-09 18:40:20 +0000

    ENH: More robust model tracking

--- django_resilient_webhook/signals.py
+++ django_resilient_webhook/signals.py
@@ -1,11 +1,7 @@
-from django.db.models.signals import m2m_changed, post_delete, post_save
+from django.db.models.signals import m2m_changed, post_save, pre_delete
 from django.dispatch import Signal
 
 from django_resilient_webhook.utilities.model_serializer import serialize_model_data
-
-
-# from django_resilient_webhook.models import Webhook
-# from django.dispatch import receiver
 
 
 drw_event_receive = Signal()
@@ -13,11 +9,32 @@ drw_event_parse_success = Signal()
 drw_event_parse_fail = Signal()
 
 
-def save_webhookable_subclass(created, **kwargs):
-    if created:
-        create_webhookable_subclass(created=created, **kwargs)
-    else:
-        update_webhookable_subclass(created=created, **kwargs)
+def save_webhookable_subclass(**kwargs):
+    has_created_kwarg = "created" in kwargs
+    if has_created_kwarg and kwargs["created"]:
+        create_webhookable_subclass(**kwargs)
+    elif has_created_kwarg and not kwargs["created"]:
+        update_webhookable_subclass(**kwargs)
+    elif not has_created_kwarg:
+        if hasattr(kwargs["instance"], "webhooks"):
+            # This is m2m_changed
+            # NOTE: We will use m2m_changed to notify when webhook is assigned to an object
+            # This can be used as a 'create' (pseudo) event w.r.t an external service
+            # print("in 3rd case", kwargs, kwargs["instance"], kwargs["instance"].webhooks.all())
+            #
+            # We cannot assume removing a webhook to be deletion so do nothing then
+
+            if kwargs["action"] == "pre_clear":
+                # Do not do anything when the m2m relation is removed
+                # print("pre_clear", kwargs["instance"].webhooks.all(), kwargs["pk_set"])
+                pass
+            elif kwargs["action"] == "post_add":
+                added_webhookes = kwargs["instance"].webhooks.filter(pk__in=kwargs["pk_set"])
+                create_webhookable_subclass(instance=kwargs["instance"], webhooks=added_webhookes)
+            elif kwargs["action"] == "pre_remove":
+                # Do not do anything when the m2m relation is removed
+                # print("pre_remove", kwargs["instance"].webhooks.all(), kwargs["pk_set"])
+                pass
 
 
 def trigger_webhook(webhooks, label, data):
@@ -32,23 +49,23 @@ def create_webhookable_subclass(instance, **kwargs):
     # This means we need to create the webhookable mode instance first before assigining
     # webhook to it. So, the object already exists at the time.
     serialized_data = serialize_model_data(instance, instance.WEBHOOK_SERIALIZED_FIELDS)
-    trigger_webhook(instance.webhooks.all(), "create", serialized_data)
+    trigger_webhook(kwargs.get("webhooks", instance.webhooks.all()), "create", serialized_data)
 
 
 def update_webhookable_subclass(instance, **kwargs):
     serialized_data = serialize_model_data(instance, instance.WEBHOOK_SERIALIZED_FIELDS)
-    trigger_webhook(instance.webhooks.all(), "update", serialized_data)
+    trigger_webhook(kwargs.get("webhooks", instance.webhooks.all()), "update", serialized_data)
 
 
 def delete_webhookable_subclass(instance, **kwargs):
     serialized_data = serialize_model_data(instance, instance.WEBHOOK_SERIALIZED_FIELDS)
-    trigger_webhook(instance.webhooks.all(), "delete", serialized_data)
+    trigger_webhook(kwargs.get("webhooks", instance.webhooks.all()), "delete", serialized_data)
 
 
 def connect_signals_to_class(cls):
     post_save.connect(save_webhookable_subclass, sender=cls, weak=False)
-    post_delete.connect(delete_webhookable_subclass, sender=cls, weak=False)
-    m2m_changed.connect(save_webhookable_subclass, sender=cls, weak=False)
+    pre_delete.connect(delete_webhookable_subclass, sender=cls, weak=False)
+    m2m_changed.connect(save_webhookable_subclass, sender=cls.webhooks.through, weak=False)
 
 
 # @receiver(pre_save, sender=Webhook)
