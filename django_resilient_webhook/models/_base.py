7f93bd5f0694af797e5db7a773951191a095d75e
Naveen Anil <devons.horizon@gmail.com>
2025-03-09 15:44:25 +0000

    WIP: Implement signal based model webhook poster

--- django_resilient_webhook/models/_base.py
+++ django_resilient_webhook/models/_base.py
@@ -1,9 +1,33 @@
 from django.contrib.contenttypes.fields import GenericRelation
-from django.db.models import Model
+from django.db.models import Model, TextField
+
+from django_resilient_webhook.signals import connect_signals_to_class
+
+
+ALLOWED_WEBHOOK_EVENTS = ["create", "update", "delete"]
 
 
 class WebhookableModel(Model):
-    webhook_integrations = GenericRelation("django_resilient_webhook.Webhook")
+    WEBHOOK_EVENTS = []
+
+    webhooks = GenericRelation("django_resilient_webhook.Webhook")
+
+    def __init_subclass__(cls, **kwargs):
+        connect_signals_to_class(cls)
+        super().__init_subclass__(**kwargs)
+
+    def save(self, *args, **kwargs):
+        errored = [event for event in self.WEBHOOK_EVENTS if event not in ALLOWED_WEBHOOK_EVENTS]
+        if errored:
+            raise TypeError(f"Event{'s' if len(errored) > 1 else ''} not allowed as WEBHOOK_EVENTS. {errored}")
+
+        super().save(*args, **kwargs)
 
     class Meta:
         abstract = True
+
+
+class Player(WebhookableModel):
+    WEBHOOK_EVENTS = ["create"]
+
+    name = TextField()
--- django_resilient_webhook/signals.py
+++ django_resilient_webhook/signals.py
@@ -1,6 +1,34 @@
-import django.dispatch
+from django.db.models.signals import m2m_changed, post_delete, post_save
+from django.dispatch import Signal
 
 
-drw_event_receive = django.dispatch.Signal()
-drw_event_parse_success = django.dispatch.Signal()
-drw_event_parse_fail = django.dispatch.Signal()
+drw_event_receive = Signal()
+drw_event_parse_success = Signal()
+drw_event_parse_fail = Signal()
+
+
+def save_webhookable_subclass(created, **kwargs):
+    if created:
+        create_webhookable_subclass(created=created, **kwargs)
+    else:
+        update_webhookable_subclass(created=created, **kwargs)
+
+
+def create_webhookable_subclass(**kwargs):
+    print("create_handler", kwargs)
+
+
+def update_webhookable_subclass(**kwargs):
+    print("update_handler", kwargs)
+    pass
+
+
+def delete_webhookable_subclass(**kwargs):
+    print("delete_handler", kwargs)
+    pass
+
+
+def connect_signals_to_class(cls):
+    post_save.connect(save_webhookable_subclass, sender=cls, weak=False)
+    post_delete.connect(delete_webhookable_subclass, sender=cls, weak=False)
+    m2m_changed.connect(save_webhookable_subclass, sender=cls, weak=False)
